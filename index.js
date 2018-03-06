1d642199d08f235c55a6b6986632fa3d20cf5e91
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-06 20:01:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function theBeatlesPlay(arr, arr2) {
   var finArr = [];
 
   for(let i = 0; i < arr.length; i++) {
-    var str = `${arr[i]} plays ${arr2[i]}`);
-    finArr.push(str);
+    finArr.push(`${arr[i]} plays ${arr2[i]}`);
     return finArr;
   }
 }
